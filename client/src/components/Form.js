f77e44219955310ddeba1284105b2a372ab94625
grahamhub <grammar@hey.com>
2021-06-09 12:16:51 -0500

    form component refactor

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,41 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from './Input';
 
 const Form = ({
-  formType,
-  title,
-  price,
-  quantity,
-  handleCancelClick,
-  handleFormSubmit,
-  handleFormUpdate,
+  type,
+  title = "New Form",
+  product = {},
+  submitHandler = (_) => null,
+  handleCancelClick = (_) => null,
 }) => {
+  let formClass = type + "-form";
+  let submitText = type === "add" ? "Add" : "Update";
+
+  const [productName, setProductName] = useState(product.title || "");
+  const [productPrice, setProductPrice] = useState(product.price || "0.00");
+  const [productQty, setProductQty] = useState(product.quantity || "0");
+
+  /**
+   * Prevent the event's default behavior and pass the
+   * formatted request body to the handler
+   */
+  const handleFormSubmit = (ev) => {
+    ev.preventDefault();
+    let newProduct = {
+      _id: product._id,
+      title: productName,
+      price: productPrice,
+      quantity: productQty,
+    }
+    submitHandler(newProduct);
+  };
 
   return (
-    <div className={`${formType}-form`}>
-      <h3>{formType === "add" ? "Add" : "Edit"} Product</h3>
+    <div className={formClass}>
+      <h3>{title}</h3>
       <form>
-        <div className="input-group">
-          <label for="product-name">Product Name</label>
-          <input type="text" id="product-name" value={title} onChange={handleFormUpdate} />
-        </div>
-
-        <div className="input-group">
-          <label for="product-price">Price</label>
-          <input type="text" id="product-price" value={price} onChange={handleFormUpdate} />
-        </div>
-
-        <div className="input-group">
-          <label for="product-quantity">Quantity</label>
-          <input type="text" id="product-quantity" value={quantity} onChange={handleFormUpdate} />
-        </div>
-
-        <div className="actions form-actions">
-          <a className="button" onClick={handleFormSubmit}>{formType === "add" ? "Add" : "Update"}</a>
-          <a className="button" onClick={handleCancelClick}>
-            Cancel
-          </a>
-        </div>
+      <Input
+        title="Product Name"
+        value={productName}
+        label="product-name"
+        setState={setProductName}
+      />
+      <Input
+        title="Price"
+        value={productPrice}
+        setState={setProductPrice}
+      />
+      <Input
+        title="Quantity"
+        value={productQty}
+        setState={setProductQty}
+      />
       </form>
+      <div className="actions form-actions">
+        <button className="button" onClick={handleFormSubmit}>{submitText}</button>
+        <button className="button" onClick={handleCancelClick}>Cancel</button>
+      </div>
     </div>
   );
 };
